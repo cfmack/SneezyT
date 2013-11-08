17432f5e609a6cc775b1441f2752457a769dfc8a
cmack <cfm0423@yahoo.com>
2013-11-07 20:52:37 -0600

    Visual drop downs do not click on Firefox on Mac

--- application/views/nav_view.php
+++ application/views/nav_view.php
@@ -21,7 +21,40 @@
 	<link rel=stylesheet href="<?php echo base_url();?>public/css/sneezy.css" type="text/css" />
 
 
-    <script data-main="public/main" src="<?php echo base_url();?>public/require.js"></script>
+    <script src="<?php echo base_url();?>public/require.js"></script>
+    <script>
+        // ... but be aware that the data-main script is loaded asynchronously.
+        require.config({
+            baseUrl: 'public',
+            deps: ["main"],
+            paths: {
+                "jquery"        :   'lib/jquery-ui/js/jquery-1.9.1',
+                "jquery-ui"     :   'lib/jquery-ui/js/jquery-ui-1.10.2.custom.min',
+                "timepicker"    :   'lib/jquery-ui/js/jquery-ui-timepicker-addon',
+                "jtable"        :   'lib/jtable/jquery.jtable.min',
+                "fullcalendar"  :   'lib/fullcalendar/fullcalendar.min',
+                "bootstrap"     :   "lib/bootstrap/js/bootstrap",
+                "timeline"      :   "http://static.simile.mit.edu/timeline/api/timeline-api"
+            },
+            shim: {
+                "jquery-ui": {
+                    exports: "$",
+                    deps: ['jquery']
+                },
+                "timepicker" : {
+                    deps: ['jquery', "jquery-ui"]
+                },
+                "jtable" : {
+                    deps: ['jquery', "jquery-ui"]
+                },
+                "fulltable" : {
+                    deps: ['jquery', "jquery-ui"]
+                },
+                "bootstrap": ['jquery']
+            }
+        });
+    </script>
+
 </head>
 <body >
 <div class="navbar navbar-inverse">
