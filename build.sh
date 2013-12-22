a01abe0e401a1392d118a4b0f506e0ddb275cf9d
cmack <cfm0423@yahoo.com>
2013-12-21 22:09:56 -0600

    Script the build process

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ZIPFILE="sneezy."$RANDOM".tar.gz"
+
+cd /home/cmack
+rm -f $ZIPFILE
+rm -Rf temp_copy
+mkdir temp_copy
+cp -Rf SneezyT/* temp_copy/
+rm -Rf temp_copy/ddl
+rm -Rf temp_copy/r
+rm -Rf temp_copy/user_guide
+rm -Rf temp_copy/.git
+rm -Rf temp_copy/.idea
+rm -Rf temp_copy/.gitignore
+mv temp_copy/application/config/database.php temp_copy/application/config/database.php.temp
+mv temp_copy/application/config/config.php temp_copy/application/config/config.php.temp
+cd temp_copy
+tar -zcvf ../$ZIPFILE ./
+cd ..
+rm -Rf temp_copy
+echo $ZIPFILE
