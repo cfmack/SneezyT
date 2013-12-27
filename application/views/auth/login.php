bb0a173d83708fcfc48d6b448cb4ae4c85309ce3
cmack <cfm0423@yahoo.com>
2013-12-26 22:53:58 -0600

    Title Page and Secondary Page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -2,42 +2,47 @@
     $this->view('metadata');
 ?>
 <div id="login-container">
-    <div class="navbar">
+    <div class="navbar" style="">
         <div class="navbar-inner">
-            <div class="container">
+            <div class="container" >
                 <a class="brand" href="#">Sneezy T</a>
             </div>
         </div>
     </div>
     <div class="login-inner-container">
-
-        <div id="infoMessage"><?php echo $message;?></div>
-
-        <?php echo form_open("welcome/login");?>
-          <fieldset class="long-inputs">
-              <div class="ui-widget">
-                <?php echo lang('login_identity_label', 'identity');?>
-                <?php echo form_input($identity);?>
-              </div>
-
-              <div class="ui-widget">
-                <?php echo lang('login_password_label', 'password');?>
-                <?php echo form_input($password);?>
-              </div>
-
-              <div class="ui-widget">
-                <?php echo lang('login_remember_label', 'remember');?>
-                <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-              </div>
-          </fieldset>
-
-          <div class="login-button-container" ><?php echo form_submit('submit', lang('login_submit_btn'));?></div>
-
-        <?php echo form_close();?>
-        <div class="login-link-container" >
-            <a href="../auth/forgot_password"><?php echo lang('login_forgot_password');?></a><br />
-            <a href="../auth/create_user"><?php echo lang('login_register');?></a>
+        <div id="login-about" class="hidden-phone">
+            <?php
+                $this->view('help/logon_about');
+            ?>
         </div>
+        <div id="login-form" >
+            <div id="infoMessage"><?php echo $message;?></div>
 
+            <?php echo form_open("welcome/login");?>
+              <fieldset class="long-inputs">
+                  <div class="ui-widget">
+                    <?php echo lang('login_identity_label', 'identity');?>
+                    <?php echo form_input($identity);?>
+                  </div>
+
+                  <div class="ui-widget">
+                    <?php echo lang('login_password_label', 'password');?>
+                    <?php echo form_input($password);?>
+                  </div>
+
+                  <div class="ui-widget">
+                    <?php echo lang('login_remember_label', 'remember');?>
+                    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+                  </div>
+              </fieldset>
+
+              <div class="login-button-container" ><?php echo form_submit('submit', lang('login_submit_btn'));?></div>
+
+            <?php echo form_close();?>
+            <div class="login-link-container" >
+                <a href="../auth/forgot_password"><?php echo lang('login_forgot_password');?></a><br />
+                <a href="../auth/create_user"><?php echo lang('login_register');?></a>
+            </div>
+        </div>
     </div>
 </div>
---
+++ application/views/help/logon_about.php
@@ -0,0 +1,2 @@
+<h1>Sneezy T's Food Tracker</h1>
+This app is focused on helping those people trying to pinpoint Food Allergies and Eosinophilic Esophagitis (EoE).
