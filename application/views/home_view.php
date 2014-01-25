d1e342c324372210e26ce13926bdf8c5013a394f
cmack <cfm0423@yahoo.com>
2014-01-24 20:59:08 -0600

    Update log on and home descriptions

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -3,15 +3,13 @@
         <div class="category-inner-left hidden-phone">
             <h1>Sneezy T's Food Tracker</h1>
             <p>
-                This is an app that tracks food and symptoms associated with Food Allergies and Gastrointestinal Disorders, such as Eosinophilic Esophagitis (EoE).  With this application, you can track the diet of multiple
-                people and their reactions.  Reactions include vomiting, hives, or other external symptoms.  Additionally, you can track the medicine used to treat a reaction and environmental pollens
+                This website tracks food and symptoms associated with Food Allergies and Gastrointestinal Disorders, such as Eosinophilic Esophagitis (EoE). A food diary and symptom tracker, such as this application, can help you decide what is plaguing your body. Using this program, you can track the type of food or beverage, the time you ate or drank it, the amount and any notes you would like to include. Additionally, you can track symptoms, the time you experienced them and how long they lasted. Also, you can keep track of your medicine, treatment and environmental factors that might play a role into your health. Likewise, each user can register multiple people to track in this application.
             </p>
             <p>
-                This app is currently provided by a family who has personal struggles with EoE and food allergies of our son.  You can read about our adventures on our blog: <a href="sneezty.wordpress.com">sneezty.wordpress.com</a>
-            </p>
+                We created the Sneezy T application to help manage our son’s multiple food allergies and his Eosinophilic Esophagitis (EoE). You can read about our adventures, find recipes and resources for food allergies and Eosinophilic Esophagitis (EoE) on our blog: <a href="www.sneezty.wordpress.com">www.sneezty.wordpress.com</a>
             </p>
             <p>
-                Hosting this costs money but we do this to help the community at large.  If you would like contribute code, I have open sourced the entire package.  Please reach out. If you would like to donate to keep this site going, please do so at our Paypal account.
+                We have spent hundreds of hours creating this application and blog in our free time in the hopes that we can help others who are in our same boat. We’d LOVE to have people contribute code to help make this a better product. We have open sourced the entire package. Please reach out, if you want to make this better. If you like this application, a recipe, or some comic relief that you’ve received as a result of our efforts, please donate money to our paypal account to help us keep up the administrative fees associated with hosting this site. Half of all the donations to our paypal account will be sent to the <a href="http://apfed.org/drupal/drupal/index.php">American Partnership for Eosinophilic Disorders (APFED)</a> in the hopes that we will find a cure for those suffering with Eosinophilic Disorders.
             </p>
         </div>
         <div class="category-inner-right" >
