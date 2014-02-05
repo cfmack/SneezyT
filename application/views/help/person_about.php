0d9d21f243eb488187fbc0a7df0f7725dd75869f
cmack <cfm0423@yahoo.com>
2014-02-04 23:12:40 -0600

    Clean up Person admin

---
+++ application/views/help/person_about.php
@@ -0,0 +1,12 @@
+<h1><i class="fa fa-user"></i> Person</h1>
+<p>
+Sneezy T allow you to track multiple users.  By default, when you sign up, you are given a single person to track.
+This mirrors your log in information.   You can certainly change this with the Edit button in the list below.
+</p>
+<p>
+    If you would like to change the user you are tracking in this session, use the Change Person option in the menu.
+</p>
+<p>
+    To change the user you would like to load by default, you can accomplish this two ways.  Either add the new user with the default check box checked.
+    Or add the Edit button to change the other Person to be the default.  You have to set the value to 1.  You will also need to set the other people to 0.
+</p>
--- application/views/person_view.php
+++ application/views/person_view.php
@@ -1,26 +1,36 @@
-<h1>Add Person</h1>
-<div id="person-add-container">
-    <form action="post">
-        <fieldset  class="add-inputs">
-            <div class="ui-widget">
-                <label for="person-name">Name: </label>
-                <input id="person-name" />
-            </div>
-            <div class="ui-widget">
-                <label for="person-default">Default: </label>
-                <input id="person-default" type="checkbox"/>
-            </div>
-            <div class="ui-widget">
-                <label for="person-note">Note: </label>
-                <textarea id="person-note"></textarea>
-            </div>
-            <div id='add-person-submit' >
-                <button class="btn btn-primary" type="button">Add</button>
-            </div>
-        </fieldset>
-    </form>
+<div id="person-add-container" class="content-category-container">
+    <div class="category-inner-container">
+        <div class="category-inner-left hidden-phone">
+            <?php
+            $this->view('help/person_about');
+            ?>
+        </div>
+        <h1 class="visible-phone"><i class="fa fa-user"></i> Add Person</h1>
+        <div class="category-inner-right">
+            <form action="post">
+                <fieldset  class="add-inputs">
+                    <div class="ui-widget">
+                        <label for="person-name">Name: </label>
+                        <input id="person-name" />
+                    </div>
+                    <div class="ui-widget">
+                        <label for="person-default">Default: </label>
+                        <input id="person-default" type="checkbox"/>
+                    </div>
+                    <div class="ui-widget">
+                        <label for="person-note">Note: </label>
+                        <textarea id="person-note"></textarea>
+                    </div>
+                    <div id='add-person-submit' >
+                        <button class="btn btn-primary" type="button">Add</button>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+    <div id="person-grid"></div>
 </div>
-<div id="person-grid"></div>
+
 <div id="person-response"></div>
 <script>
     require(["modules/person"], function(person) {
