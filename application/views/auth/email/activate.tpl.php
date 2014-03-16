848e8dc187cb0105447d2601c472651a4728e477
cmack <cfm0423@yahoo.com>
2014-03-15 22:17:51 -0500

    Account Activation Email updates

--- application/views/auth/email/activate.tpl.php
+++ application/views/auth/email/activate.tpl.php
@@ -8,6 +8,12 @@
 </head>
 <body>
 <h3><?php echo sprintf(lang('email_activate_heading'));?></h3>
+<p>
+    You will need to activate your account by clicking the link below.
+</p>
+<p>
+    As a reminder, you can use Sneezy T to track food and symptoms associated with Food Allergies and Gastrointestinal Disorders, such as Eosinophilic Esophagitis (EoE). A food diary and symptom tracker, such as this application, can help you decide what is plaguing your body. Using this program, you can track the type of food or beverage, the time you ate or drank it, the amount and any notes you would like to include. Additionally, you can track symptoms, the time you experienced them and how long they lasted. Also, you can keep track of your treatment and environmental factors that might play a role into your health.
+</p>
 	<p><?php echo sprintf(lang('email_activate_subheading'), anchor('auth/activate/'. $id .'/'. $activation, lang('email_activate_link')));?></p>
 </body>
 </html>
